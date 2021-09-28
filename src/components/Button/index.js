d986fe1df976998ce05760bae752f685a11a206f
Jorge Mayoral Álvarez <mayoralalvarezj@gmail.com>
2021-09-28 10:19:33 +0200

    feat: Add button clicked style

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,16 +1,13 @@
-import { useState } from 'react';
 import './style.scss';
 
 const Button = ({ label, destructive = false, loading = false, ...props }) => {
-  const [isClicked, setIsClicked] = useState(false);
-
   const icon = `button__text__icon ${
     loading ? 'icon-arrow-sync' : destructive ? 'icon-delete' : 'icon-circle'
   }`;
 
   const className = `button ${destructive && 'button--state-destructive'} ${
     loading && 'button--state-loading'
-  } ${isClicked && 'clicked'}`;
+  }`;
 
   return (
     <button
