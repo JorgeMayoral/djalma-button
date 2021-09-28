e4ae12d32e031f80278dc27f186f8629c3cfe7bd
Jorge Mayoral Álvarez <mayoralalvarezj@gmail.com>
2021-09-28 15:14:28 +0200

    feat: Add size and color props to Icon component

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -2,13 +2,27 @@ import PropTypes from "prop-types";
 
 const proptypes = {
   icon: PropTypes.string,
+  size: PropTypes.string,
+  color: PropTypes.string,
 };
 
-const Icon = ({ icon }) => {
+const sizes = {
+  medium: "2.4rem",
+  large: "4.8rem",
+};
+
+const colors = {
+  neutral: "#FAFAFA",
+  error: "#E43E42",
+  accent: "#594794",
+};
+
+const Icon = ({ icon, size = "medium", color = "neutral" }) => {
   return (
-    <div>
-      <span className={icon} />
-    </div>
+    <span
+      className={`${icon}`}
+      style={{ fontSize: sizes[size], color: colors[color] }}
+    />
   );
 };
 
